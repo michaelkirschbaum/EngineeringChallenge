99923fff27cf9b77d8c5b889cf26f2f53f2fd29c
Michael Kirschbaum <hi@michaelkirschbaum.com>
2023-12-23 17:42:39 -0600

    create AuthContext

--- native-app/app/login.tsx
+++ native-app/app/login.tsx
@@ -1,4 +1,9 @@
-import {View} from '../../components/Themes';
+import {StyleSheet} from 'react-native';
+import {createContext, useContext} from 'react';
+
+import {View} from '../components/Themed';
+
+export const AuthContext = createContext({ authState: {id: "", username: "", signedIn: false}, setAuthState: () => {} });
 
 export default function LoginScreen() {
   return (
