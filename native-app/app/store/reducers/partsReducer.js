b695514db542ad8bebdbd8ab762d53ff3c4d533b
Michael Kirschbaum <hi@michaelkirschbaum.com>
2023-12-26 02:31:34 -0600

    implement addPart reducer case

--- native-app/app/store/reducers/partsReducer.js
+++ native-app/app/store/reducers/partsReducer.js
@@ -1,16 +1,28 @@
 import {createReducer, createAction} from '@reduxjs/toolkit';
 
-const initialState = [];
+const initialState = {
+  machines: null
+};
 
-const addParts = createAction('ADD_PARTS');
-const resetParts = createAction('RESET_PARTS');
+const addPart = createAction('ADD_PART');
+const addScore = createAction('ADD_SCORE');
+const resetMachineData = createAction('RESET_MACHINE_DATA');
 
 export default partsReducer = createReducer(initialState, (builder) => {
   builder
-    .addCase(addParts, (state, action) => {
-      return state;
+    .addCase(addScore, (state, action) => {})
+    .addCase(addPart, (state, action) => {
+      return {
+        machines: {
+          ...state.machines,
+          [action.payload.machineName]: {
+            ...state.machines[action.payload.machineName],
+            [action.payload.partName]: action.payload.partValue
+          }
+        }
+      };
     })
-    .addCase(resetParts, (state, action) => {
-      return state;
+    .addCase(resetMachineData, (state, action) => {
+      return initialState;
     })
 });
