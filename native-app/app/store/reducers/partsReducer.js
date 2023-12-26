b3afd3ffd0e68d98a523369e37b75994da337846
Michael Kirschbaum <hi@michaelkirschbaum.com>
2023-12-25 21:56:08 -0600

    add reducer cases

--- native-app/app/store/reducers/partsReducer.js
+++ native-app/app/store/reducers/partsReducer.js
@@ -1,9 +1,16 @@
 import {createReducer, createAction} from '@reduxjs/toolkit';
 
 const initialState = [];
+
+const addParts = createAction('ADD_PARTS');
 const resetParts = createAction('RESET_PARTS');
 
 export default partsReducer = createReducer(initialState, (builder) => {
   builder
-    .addCase(resetParts, (state, action) => {})
+    .addCase(addParts, (state, action) => {
+      return state;
+    })
+    .addCase(resetParts, (state, action) => {
+      return state;
+    })
 });
