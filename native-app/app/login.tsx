efd5eb4eff41170b9334452d406bb2e108c9e138
Michael Kirschbaum <hi@michaelkirschbaum.com>
2023-12-27 16:09:19 -0600

    add text field to login screen

--- native-app/app/login.tsx
+++ native-app/app/login.tsx
@@ -1,14 +1,39 @@
 import {StyleSheet} from 'react-native';
 import {createContext, useContext} from 'react';
+import {useState} from 'react';
 
-import {View} from '../components/Themed';
 
 export const AuthContext = createContext({ authState: {id: "", username: "", signedIn: false}, setAuthState: () => {} });
+import {View, Text} from '../components/Themed';
 
 export default function LoginScreen() {
+  const [username, setUsername] = useState('');
+
   return (
-    <View></View>
+    <View>
+        <Text style={styles.label}>Username</Text>
+        <TextInput
+          style={styles.input}
+          value={username}
+          onChangeText={(text) => setUsername(text)}
+          placeholder='Enter username'
+        />
+
+        <Button title='Login' onPress={login} />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({});
+  label: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+  },
+});
